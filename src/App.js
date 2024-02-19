c3ddfa4e11fcb984849538e3f47936c84bb4f505
Onurakgull <65688719+onurakgl@users.noreply.github.com>
2024-02-19 23:25:41 +0300

    Css Öncesi Son Düzenlemeler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import City from './City';
+
 
 function App() {
+  const key = "ad178e8f5afe306c6370995d6862e25f";
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState();
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`);
+        console.log(response);
+        console.log(response.data)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi()
+  },[search])
+  console.log(search);
   return (
     <div className="App">
-      Hello
+      Hello <br></br>
+      <input onChange={(e) => setSearch(e.target.value)} className='border-8 bg-slate-500' type="text"/>
+      <City city={city}/>
     </div>
   );
 }
