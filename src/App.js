e1d1b92d18cf1bcf4f386c7d408913b6337326b4
Onurakgull <65688719+onurakgl@users.noreply.github.com>
2024-02-20 00:24:24 +0300

    Css Öncesi Sorunların Giderilmesi

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       try {
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`);
         console.log(response);
-        console.log(response.data)
+        setCity(response.data)
       } catch (error) {
         console.error(error);
       }
@@ -23,9 +23,9 @@ function App() {
   console.log(search);
   return (
     <div className="App">
-      Hello <br></br>
+      How's The Weather ?<br></br>
       <input onChange={(e) => setSearch(e.target.value)} className='border-8 bg-slate-500' type="text"/>
-      <City city={city}/>
+      {city && <City city={city} />}
     </div>
   );
 }
